f82291644a3f31d97f21f52158ffe27d672faede
kemalhiero <kemalhiero@gmail.com>
2022-07-05 15:17:12 +0700

    awal

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# safditeknnik
+# Tugas Besar PBD-Safdi Teknik
+
+- Tabel Relasi
+<img src="tabelrelasi.jpeg">
+.<br><br>
+    -- Admin --
+    1. Admin Mendaftarkan Pelanggan
+    2. Admin Mendaftarkan Barang
+    3. Admin
+    <br><br>
+
+   -- Teknisi --
+   1. Teknisi Dapat Mengupdate Status Perbaikan Barang
+<br><br>
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+dotenv.config();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+
+app.get("/", (req, res) => {
+    res.send('oppppp')
+});
+
+
+app.listen(port, () => {
+  console.log(`Server Sedang Berjalan di http://localhost:${port}`);
+});
